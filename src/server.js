b625c108a3e013aa98a316f8984d18e883f3c53b
Bryann <bryann@brovia.com>
2019-08-26 04:04:14 +0200

    s3 upload

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import "./passport";
 import { authenticateJwt } from "./passport";
 import "./passport";
 import schema from "./schema";
+import { uploadMiddleware, uploadController } from "./upload";
 
 const PORT = process.env.PORT || 4000;
 
@@ -16,6 +17,7 @@ const server = new GraphQLServer({
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 
 server.start({ port: PORT }, () =>
   console.log(`Server running on  http://localhost:${PORT}`)
